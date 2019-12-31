681e9a7b2b91431e0808730496102cf64a8fd9b0
Mehmet Sunkur <mehmet.sunkur@aurea.com>
2019-12-31 12:21:20 +0300

    commit5

--- src/main/java/io/github/jhipster/application/ApplicationWebXml.java
+++ src/main/java/io/github/jhipster/application/ApplicationWebXml.java
@@ -15,6 +15,7 @@ public class ApplicationWebXml extends SpringBootServletInitializer {
         /**
          * set a default to use when no profile is configured.
          */
+        
         DefaultProfileUtil.addDefaultProfile(application.application());
         return application.sources(JhipsterJenkinsApplicationApp.class);
     }
